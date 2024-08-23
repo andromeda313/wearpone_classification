ec7f4ca090705dd2d70af922f5cdc7c10bfda996
andromeda313 <102717269+andromeda313@users.noreply.github.com>
2024-08-23 15:02:58 +0500

    add

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+import platform
+
+plt = platform.system()
+if plt =='Linux':
+    pathlib.WindowsPath = pathlib.PosixPath
+
+st.title("Qurollarni classiffikasiya qilish")
+
+file = st.file_uploader("Rasmni yuklash", 
+                   type = ['png',
+                           'jpeg', 
+                           'jpg', 
+                           'gif', 
+                           'svg'])
+
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('wearpons_model.pkl')
+    
+    prediction, pred_id, prob = model.predict(img)
+    st.success(f"Bashorat:{prediction}")
+    st.info(f'Ehtimollik: {prob[pred_id]*100:.1f} %')
+    
+    fig = px.bar(x = prob*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
